7a0fc3f7df65d4199e1e433ace2b269c42b37143
Amine Afia <amine.afia@fr.ibm.com>
2016-05-15 22:22:13 +0200

    delete gh-pages

--- dist/app/+path-graph/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var path_graph_component_1 = require('./path-graph.component');
-exports.PathGraphComponent = path_graph_component_1.PathGraphComponent;
-//# sourceMappingURL=index.js.map
--- dist/app/+paths/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var paths_component_1 = require('./paths.component');
-exports.PathsComponent = paths_component_1.PathsComponent;
-//# sourceMappingURL=index.js.map
